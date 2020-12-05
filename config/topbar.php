02a1df20eca6880289d8359a1de0b261548c63e1
ekza97 <ekza97@gmail.com>
2020-12-05 19:30:46 +0900

    Disable ubah password

--- config/topbar.php
+++ config/topbar.php
@@ -24,10 +24,10 @@
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a> -->
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#gantiPasswordModal">
+                <!-- <a class="dropdown-item" href="#" data-toggle="modal" data-target="#gantiPasswordModal">
                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                     Ganti Password
-                </a>
+                </a> -->
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
